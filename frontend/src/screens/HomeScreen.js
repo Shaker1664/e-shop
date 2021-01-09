debfad8da127ca22f55c835516c059686e7a87d8
Shaker1664 <shakerkamal@iut-dhaka.edu>
2021-01-09 16:42:49 +0600

    Added Rating and Price for each Product

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,7 +9,7 @@ const HomeScreeen = () => {
             <h1>Latest Products</h1>
             <Row>
                 {products.map((product) => (
-                    <Col sm={12} md={6} lg={4} lx={3}>
+                    <Col key={product._id} sm={12} md={6} lg={4} lx={3}>
                     <Product product={product} />
                     </Col>
                 ))}
