659f072cd5fb4f72141a8702aa049f81851d3548
Shaker1664 <shakerkamal@iut-dhaka.edu>
2021-01-12 22:36:09 +0600

    Added Models using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import products from './data/products.js'
 
+dotenv.config()
+
+connectDB()
 
 const app = express();
 
@@ -17,4 +22,9 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product);
 });
 
-app.listen(5000, console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000
+
+app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
